cbe6f73544656572fc83d655d725268caf404b97
Damon Oehlman <damon.oehlman@gmail.com>
2013-10-08 16:36:15 +1100

    Updated shared-pointer demo

--- examples/shared-pointer.js
+++ examples/shared-pointer.js
@@ -3,8 +3,40 @@ var pull = require('pull-stream');
 var observable = require('pull-observable');
 var dc = require('../');
 
+// create a canvas to draw the remote line
+var canvas = document.createElement('canvas');
+var context = canvas.getContext('2d');
+
+// initialise the canvas height and width and add to the doc
+canvas.width = 640;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
 // capture the pointer on the document
-var point = require('point')(document);
+var point = require('point')(canvas);
+
+function drawCursor(color) {
+  var lastXY = [];
+
+  return function(args) {
+    var xy = args.slice(0, 2);
+    var data = args[2];
+
+    if (data.type === 'move') {
+      context.strokeStyle = color;
+      context.beginPath();
+      context.moveTo.apply(context, lastXY);
+      context.lineTo.apply(context, xy);
+      context.stroke();
+      context.closePath();
+    }
+
+    // save the last xy
+    lastXY = [].concat(xy);
+
+    return args;
+  };
+}
 
 quickconnect({ ns: 'dctest', data: true })
   .on('dc:open', function(channel, peerId) {
@@ -13,6 +45,7 @@ quickconnect({ ns: 'dctest', data: true })
     // stream the pointer information across the datachannel
     pull(
       observable(point),
+      pull.map(drawCursor('#000')),
       pull.map(JSON.stringify),
       dc.write(channel)
     );
@@ -21,6 +54,6 @@ quickconnect({ ns: 'dctest', data: true })
     pull(
       dc.read(channel),
       pull.map(JSON.parse),
-      pull.log()
+      pull.drain(drawCursor('#F00'))
     );
   });
